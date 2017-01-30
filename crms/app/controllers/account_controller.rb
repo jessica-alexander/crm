d40929bbd7efea7c2719f073bd345415fc4eaac6
Jessica <jhai_alex@hotmail.com>
2017-01-30 09:57:54 -0500

    added lead, account and appointment controllers

---
+++ crms/app/controllers/account_controller.rb
@@ -0,0 +1,13 @@
+class AccountController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- crms/config/routes.rb
+++ crms/config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get 'account/index'
+
+  get 'account/show'
+
+  get 'account/new'
+
+  get 'account/edit'
+
+  get 'appointment/index'
+
+  get 'appointment/show'
+
+  get 'appointment/new'
+
+  get 'appointment/edit'
+
+  get 'lead/index'
+
+  get 'lead/show'
+
+  get 'lead/new'
+
+  get 'lead/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
