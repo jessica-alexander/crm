17e4c9a8534fa7fdb6096348e80f40c2add27a6c
Jessica <jhai_alex@hotmail.com>
2017-01-30 10:55:56 -0500

    added routes for all the controllers

--- crms/app/controllers/appointment_controller.rb
+++ crms/app/controllers/appointment_controller.rb
@@ -1,13 +1,48 @@
 class AppointmentController < ApplicationController
   def index
+    @appointments = Account.all
   end
 
   def show
+    @account = Account.find(params[:id])
   end
 
   def new
+    @account= Account.new
   end
 
   def edit
+    @account = Account.find(params[:id])
   end
-end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      redirect_to accounts_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @account = Account.find(params[:id])
+
+    if @account.update_attributes(product_params)
+      redirect_to account_url(@account)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    redirect_to accounts_url
+  end
+
+  private
+  def account_params
+    params.require(:account).permit(:name, :phone_number, :notes)
+  end
+  end
--- crms/config/routes.rb
+++ crms/config/routes.rb
@@ -1,27 +1,6 @@
 Rails.application.routes.draw do
-  get 'account/index'
 
-  get 'account/show'
-
-  get 'account/new'
-
-  get 'account/edit'
-
-  get 'appointment/index'
-
-  get 'appointment/show'
-
-  get 'appointment/new'
-
-  get 'appointment/edit'
-
-  get 'lead/index'
-
-  get 'lead/show'
-
-  get 'lead/new'
-
-  get 'lead/edit'
+  resources :accounts, :leads, :appointments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
