f35db4d5fff7a1078127c6bcbc64031eb246eb96
Jessica <jhai_alex@hotmail.com>
2017-01-30 19:36:33 -0500

    nested call logs in leads

--- crm/config/routes.rb
+++ crm/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
 root 'leads#index'
-resources :leads
+resources :leads do
+  resources :call_logs, only: [:create, :show, :destroy, :index]
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
