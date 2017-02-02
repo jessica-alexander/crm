d6b272905a637dc61ad07ec5dfd8db72a8b37747
Jessica <jhai_alex@hotmail.com>
2017-02-02 09:41:07 -0500

    added lead_id column to appointments model

--- crm/db/schema.rb
+++ crm/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131041116) do
+ActiveRecord::Schema.define(version: 20170202143850) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "lead_id"
   end
 
   create_table "call_logs", force: :cascade do |t|
