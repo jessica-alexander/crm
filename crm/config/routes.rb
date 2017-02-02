1cecdef96fb4e0507fc86658b016394a4e2b73a9
Jessica <jhai_alex@hotmail.com>
2017-02-02 12:14:12 -0500

    nested appointments under leads

--- crm/config/routes.rb
+++ crm/config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'appointments/index'
-
-  get 'appointments/show'
-
-  get 'appointments/new'
-
-  get 'appointments/edit'
-
 root 'leads#index'
+
 resources :leads do
-  resources :call_logs, only: [:create, :show, :destroy, :index]
+  resources :call_logs
+  resources :appointments
 end
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
