82d5ade180880d884d59e4b9e3257a17fb5f083d
Jessica <jhai_alex@hotmail.com>
2017-02-03 15:58:47 -0500

    added methods to the controller

--- crm/app/controllers/accounts_controller.rb
+++ crm/app/controllers/accounts_controller.rb
@@ -1,13 +1,50 @@
 class AccountsController < ApplicationController
+
   def index
+  @accounts = Account.all
   end
 
   def show
+    @account = Account.find(params[:id])
+    # @call_log = Call_Log.new()
   end
 
   def new
+    @account = Account.new
   end
 
   def edit
+    @account = Account.find(params[:id])
   end
-end
+
+  def create
+    @account = Account.new(lead_params)
+
+    if @acount.save
+      redirect_to account_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @account = Account.find(params[:id])
+
+    if @account.update_attributes(account_params)
+      redirect_to account_url(@account)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    redirect_to accounts_url
+  end
+
+  private
+  def account_params
+    params.require(:account).permit(:first_name, :last_name, :phone_number, :notes)
+  end
+  end
