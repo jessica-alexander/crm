d1d1e6c4f45e8cf24837cef6ba009d000859823a
Jessica <jhai_alex@hotmail.com>
2017-02-02 19:49:51 -0500

    added methods

--- crm/app/controllers/appointments_controller.rb
+++ crm/app/controllers/appointments_controller.rb
@@ -22,33 +22,34 @@ class AppointmentsController < ApplicationController
   def create
     @appointment = Appointment.new(appointment_params)
     @appointment.lead = Lead.find(params[:lead_id])
-    @reservation.date = DateTime.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i, params[:date][:hour].to_i, params[:date][:minute].to_i).in_time_zone('Eastern Time (US & Canada)')
+    # @appointment.date = DateTime.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i)
 
     if @appointment.save
-      redirect_to lead_appointment_logs_path(@lead)
+      redirect_to lead_appointment_path(@lead,@appointment)
     else
       render :new
     end
   end
 
   def update
-    @appointment  = Appointment.find(params[:id])
+    @appointment = Appointment.find(params[:id])
+    @appointment.lead = Lead.find(params[:lead_id])
 
-    if  @appointment .update_attributes(lead_params)
-      redirect_to appointment_url(@appointment)
+    if  @appointment.update_attributes(appointment_params)
+      redirect_to lead_appointment_path(@lead,@appointment)
     else
       render :edit
     end
   end
 
   def destroy
-    @appointment  = CallLog.find(params[:id])
+    @appointment  = Appointment.find(params[:id])
     @appointment.destroy
   end
 
   private
   def appointment_params
-    params.require(:appointment).permit(:date, :lead_id)
+    params.require(:appointment).permit(:date, :lead_id, :time)
   end
 
   def load_lead
