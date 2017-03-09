b6be90e1ac9c93fca5afaf67d80d1d4cfe8462c9
Jessica <jhai_alex@hotmail.com>
2017-03-08 19:51:43 -0500

    added sessions controller

--- crm/config/routes.rb
+++ crm/config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
 root 'leads#index'
 
 resources :leads do
@@ -16,7 +9,7 @@ end
 
 resources :accounts
 resources :users, only: [:new, :create, :delete]
-
+resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
